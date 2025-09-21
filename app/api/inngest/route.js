eec6093c37e6e2a28e8908b070bd67a9d0206fb8
akash22bcsc05 <cse.22bcsc05@silicon.ac.in>
2025-09-21 12:40:18 +0530

    new-commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,9 +1,11 @@
+import { checkBudgetAlert } from "@/lib/inngest/functions";
+import {inngest} from "@/lib/inngest/client"; 
 import { serve } from "inngest/next";
+
+console.log("Serving Inngest route...");
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    /* your functions will be passed here later! */
-  ],
+  functions: [ checkBudgetAlert],
 });
